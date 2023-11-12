c120997ea1ae56f7aa055b0c2b1eedef615c2d71
Utkarsh Dwivedi <utkarshdwivedi3997@gmail.com>
2023-11-12 14:56:29 -0500

    Revert "fix SampleLayout to avoid build errors"
    This reverts commit aaa549b8fb437982ba60b72b68558d3461f8467e.

--- src/components/SampleLayout.tsx
+++ src/components/SampleLayout.tsx
@@ -1,21 +1,10 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { useEffect, useMemo, useRef, useState } from 'react';
 
 import type { GUI } from 'dat.gui';
 import type { Stats } from 'stats-js';
-import type { Editor, EditorConfiguration } from 'codemirror';
-interface CodeMirrorEditor extends Editor {
-  updatedSource: (source: string) => void;
-}
 
 import styles from './SampleLayout.module.css';
-
-type SourceFileInfo = {
-  name: string;
-  contents: string;
-  editable?: boolean;
-};
 
 export type SampleInit = (params: {
   canvas: HTMLCanvasElement;
@@ -23,46 +12,6 @@ export type SampleInit = (params: {
   gui?: GUI;
   stats?: Stats;
 }) => void | Promise<void>;
-
-if (process.browser) {
-  require('codemirror/mode/javascript/javascript');
-}
-
-function makeCodeMirrorEditor(source: string) {
-  const configuration: EditorConfiguration = {
-    lineNumbers: true,
-    lineWrapping: true,
-    theme: 'monokai',
-    readOnly: true,
-  };
-
-  let el: HTMLDivElement | null = null;
-  let editor: CodeMirrorEditor;
-
-  if (process.browser) {
-    el = document.createElement('div');
-    const CodeMirror = process.browser && require('codemirror');
-    editor = CodeMirror(el, configuration);
-  }
-
-  function Container(props: React.ComponentProps<'div'>) {
-    return (
-      <div {...props}>
-        <div
-          ref={(div) => {
-            if (el && div) {
-              div.appendChild(el);
-              editor.setOption('value', source);
-            }
-          }}
-        />
-      </div>
-    );
-  }
-  return {
-    Container,
-  };
-}
 
 const SampleLayout: React.FunctionComponent<
   React.PropsWithChildren<{
@@ -73,17 +22,9 @@ const SampleLayout: React.FunctionComponent<
     gui?: boolean;
     stats?: boolean;
     init: SampleInit;
-    sources: SourceFileInfo[];
   }>
 > = (props) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const sources = useMemo(
-    () =>
-      props.sources.map(({ name, contents }) => {
-        return { name, ...makeCodeMirrorEditor(contents) };
-      }),
-    props.sources
-  );
 
   const guiParentRef = useRef<HTMLDivElement | null>(null);
   const gui: GUI | undefined = useMemo(() => {
@@ -105,19 +46,9 @@ const SampleLayout: React.FunctionComponent<
     return undefined;
   }, []);
 
-  const router = useRouter();
-  const currentHash = router.asPath.match(/#([a-zA-Z0-9\.\/]+)/);
-
   const [error, setError] = useState<unknown | null>(null);
 
-  const [activeHash, setActiveHash] = useState<string | null>(null);
   useEffect(() => {
-    if (currentHash) {
-      setActiveHash(currentHash[1]);
-    } else {
-      setActiveHash(sources[0].name);
-    }
-
     if (gui && guiParentRef.current) {
       guiParentRef.current.appendChild(gui.domElement);
 
@@ -183,7 +114,7 @@ const SampleLayout: React.FunctionComponent<
           `,
           }}
         />
-        <title>{`${props.name} - WebGPU Samples`}</title>
+        <title>{`Terrain X`}</title>
         <meta name="description" content={props.description} />
         <meta httpEquiv="origin-trial" content={props.originTrial} />
       </Head>
@@ -222,36 +153,6 @@ const SampleLayout: React.FunctionComponent<
           ref={guiParentRef}
         ></div>
         <canvas ref={canvasRef}></canvas>
-      </div>
-      <div>
-        <nav className={styles.sourceFileNav}>
-          <ul>
-            {sources.map((src, i) => {
-              return (
-                <li key={i}>
-                  <a
-                    href={`#${src.name}`}
-                    data-active={activeHash == src.name}
-                    onClick={() => {
-                      setActiveHash(src.name);
-                    }}
-                  >
-                    {src.name}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-        {sources.map((src, i) => {
-          return (
-            <src.Container
-              className={styles.sourceFileContainer}
-              data-active={activeHash == src.name}
-              key={i}
-            />
-          );
-        })}
       </div>
     </main>
   );
