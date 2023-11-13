9d84a247ecc46a2b453cf2b0f61acbcec7dc9ef7
utkarshdwivedi3997 <utkarshdwivedi3997@gmail.com>
2023-11-13 21:07:57 +0000

    Deploying to gh-pages from  @ c90f1b4a24efcdf695d81ee7fcbe57463608b9ae 🚀

---
+++ _next/static/chunks/pages/index-0eb0444497c41af1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405,26],{8312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(7925)}])},7925:function(n,u,_){"use strict";_.r(u);var t=_(6652);u.default=t.default}},function(n){n.O(0,[746,784,652,774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
