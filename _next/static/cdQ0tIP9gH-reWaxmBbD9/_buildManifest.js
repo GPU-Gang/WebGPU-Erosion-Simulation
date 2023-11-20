2b44902e8731059070c2eb36235b6a622fbe281f
LinDadaism <46789205+LinDadaism@users.noreply.github.com>
2023-11-20 16:57:58 +0000

    Deploying to gh-pages from  @ 8b70a89225dc982a8d7471b083d2be9c5ba2a8e7 🚀

---
+++ _next/static/cdQ0tIP9gH-reWaxmBbD9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/784-6bbd7a3fa184f210.js","static/css/59371264f3c07eb8.css","static/chunks/pages/index-dc95dc08e72ce205.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
