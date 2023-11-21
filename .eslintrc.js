b9cf28ec65c741ee3dc19083dfd6f76ca0793ced
Utkarsh Dwivedi <utkarshdwivedi3997@gmail.com>
2023-11-20 23:05:18 -0500

    turn off these goddamn stupid rules that force bad code

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
     "react"
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
+    // "react/react-in-jsx-scope": "off",
+    // "react/prop-types": "off",
     // "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }]
   },
   globals: {
